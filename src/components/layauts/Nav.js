4060cacb88d7fe261fd8da9bda3417d74316ecda
marcelo_yugo <root@fedora>
2023-09-15 18:08:29 -0300

    Se realizan modificaciones en la clase Nav.

--- src/components/layauts/Nav.js
+++ src/components/layauts/Nav.js
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import '../../styles/componentes/layouts/nav.css';
 
-
+  
 
 const Nav = (props) => {
     return (
         <nav>
             <div class='holder'>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/tienda">Tienda</Link></li>
-                    <li><Link to="/reserva">Reserva</Link></li>
-                    <li><Link to="/contactos">Contactos</Link></li>
+                    <li><NavLink to="/" className={({ isActive }) => isActive ?  'activo' : '' }>Home</NavLink></li>
+                    <li><NavLink to="/tienda" className={({ isActive }) => isActive ?  'activo' : '' }>Tienda</NavLink></li>
+                    <li><NavLink to="/reserva" className={({ isActive }) => isActive ?  "activo" : undefined }>Reserva</NavLink></li>
+                    <li><NavLink to="/contactos" className={({ isActive }) => isActive ?  "activo" : undefined }>Contactos</NavLink></li>
                 </ul>
             </div>
         </nav>
